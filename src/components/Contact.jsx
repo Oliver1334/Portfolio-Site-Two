35348d00d7e87c75a96b9e406544cd78074ce553
Oliver1334 <oliver.taylor95@outlook.com>
2024-07-16 18:51:03 +0100

    message input character counter

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Contact = () => {
+  // state to track message character count
+  const [message, setMessage] = useState('');
+  // Function to update changes in message area
+  const handleMessageChange = (e) => {
+      setMessage(e.target.value);
+  };
+
   return (
     <div name='contact' className='w-full h-screen bg-[#040F0F] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/pbqgogdb" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#248232] text-[#FCFFFC]'>Contact</p>
-                <p className='text-[#FCFFFC] py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
+                <p className='text-[#FCFFFC] py-4'>// Submit the form below or send me an email - oliver.taylor1334@outlook.com</p>
             </div>
             <input className='p-1 bg-[#FCFFFC]' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-1 bg-[#FCFFFC]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#FCFFFC] p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <textarea className='bg-[#FCFFFC] p-2 w-full max-w-full max-h-[300px] resize-y' name="message" rows="10" placeholder='Message' value={message} onChange={handleMessageChange} maxLength="1500"></textarea>
+            <p className='text-[#FCFFFC]'>Character count: {message.length}/1500</p>
             <button className='text-white border-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] px-4 py-3 my-8 mx-auto flex items-center'> Let's Collaborate</button>
 
         </form>
 
     </div>
-  )
-}
+  );
+};
