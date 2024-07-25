499e60f4611c06dd4e15b1714549735ea5fee003
Oliver1334 <oliver.taylor95@outlook.com>
2024-07-25 19:42:39 +0100

    Carousel automatic timer

--- src/components/NCgames.jsx
+++ src/components/NCgames.jsx
@@ -1,36 +1,52 @@
-import React, {useState} from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import ncgamesimg from "../assets/ncgames.png";
-import realEstate from "../assets/realestate.jpg"
-import workImg from "../assets/workImg.jpeg"
-import { BsChevronCompactLeft, BsChevronCompactRight } from  "react-icons/bs"
+import realEstate from "../assets/realestate.jpg";
+import workImg from "../assets/workImg.jpeg";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { MdSquare } from "react-icons/md";
 
 export const Ncgames = () => {
-
   const slides = [
     { src: ncgamesimg },
     { src: realEstate },
     { src: workImg },
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slideIntervalRef = useRef(null);
 
-  const prevSlide = () => {
+  const prevSlide = useCallback(() => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
-  }
+  }, [currentIndex, slides.length]);
 
-  const nextSlide = () => {
+  const nextSlide = useCallback(() => {
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex)
-  }
+    setCurrentIndex(newIndex);
+  }, [currentIndex, slides.length]);
 
   const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
-  }
+  };
 
+  useEffect(() => {
+    slideIntervalRef.current = setInterval(() => {
+      nextSlide();
+    }, 8000); // Change slide every 3 seconds
+
+    return () => clearInterval(slideIntervalRef.current);
+  }, [nextSlide]);
+
+  useEffect(() => {
+    clearInterval(slideIntervalRef.current);
+    slideIntervalRef.current = setInterval(() => {
+      nextSlide();
+    }, 8000);
+
+    return () => clearInterval(slideIntervalRef.current);
+  }, [currentIndex, nextSlide]);
 
   return (
     <div
@@ -44,7 +60,7 @@ export const Ncgames = () => {
           </p>
           <p className="py-6">// Take a look at my most recent projects</p>
 
-          {/*body and img div*/}
+          {/* body and img div */}
           <div className="flex flex-col md:flex-row items-center md:items-start py-6">
             <p className="md:w-1/2">
               {" "}
@@ -55,39 +71,34 @@ export const Ncgames = () => {
               necessitatibus blanditiis, placeat labore architecto consequuntur.
             </p>
 
-            {/*image div*/}
+            {/* image div */}
             <div className="max-w-[600px] h-[400px] w-full m-auto relative px-8 group">
-              <div style={{ backgroundImage: `url(${slides[currentIndex].src})` }} className="w-full h-full rounded-2xl bg-center bg-cover duration-500 "> </div>
-                {/* left arrow */}
-                <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-                  <BsChevronCompactLeft onClick={prevSlide} size={30}/>
-                </div>
-                {/* right arrow */}
-                  <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-                    <BsChevronCompactRight onClick={nextSlide} size={30}/>
+              <div
+                style={{ backgroundImage: `url(${slides[currentIndex].src})` }}
+                className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+              ></div>
+              {/* left arrow */}
+              <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactLeft onClick={prevSlide} size={30} />
+              </div>
+              {/* right arrow */}
+              <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactRight onClick={nextSlide} size={30} />
+              </div>
+              <div className="flex top-4 justify-center py-2 ">
+                {slides.map((slide, slideIndex) => (
+                  <div
+                    key={slideIndex}
+                    onClick={() => goToSlide(slideIndex)}
+                    className="text-2xl cursor-pointer hover:text-[#2BA84A]"
+                  >
+                    <MdSquare />
                   </div>
-                  <div className="flex top-4 justify-center py-2 ">
-                    {slides.map((slide, slideIndex) => (
-                      <div key={slideIndex} 
-                      onClick={() => goToSlide(slideIndex)} 
-                      className="text-2xl cursor-pointer hover:text-[#2BA84A]">
-                        <MdSquare />
-                      </div>
-                    ))}
-                  </div>
-
+                ))}
+              </div>
             </div>
-
-
-
-
-
-
-
-
-
-          </div >
-          {/*buttons*/}
+          </div>
+          {/* buttons */}
           <div className="flex flex-col md:flex-row justify-left gap-12 pt-8">
             <button
               className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] 
