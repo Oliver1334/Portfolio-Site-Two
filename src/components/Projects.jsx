bf049bd02c87d99302a180792a04fa17b1d17b8e
Oliver1334 <oliver.taylor95@outlook.com>
2025-08-16 01:36:18 +0100

    Smooth scroll fixes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import weatherappimg from "../assets/weatherapp2.png";
 import ncgameslogo from "../assets/nclogo3.png";
 import fanFinder from "../assets/fanfinder.png";
@@ -8,19 +7,21 @@ import { Link } from "react-scroll";
 export const Projects = () => {
   return (
     <div
-      name="projects"
-      className="w-full md:h-screen text-[#FCFFFC] bg-[#040F0F] pt-12"
+      
+      className="w-full md:h-screen text-[#FCFFFC] bg-[#040F0F] h-[150vh] pt-32 pb-32"
     >
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full pt-12">
-        <div className="pb-4">
-          <p className="text-4xl font-bold inline border-b-4 text-[#FCFFFC] border-[#248232]">
+      <div  className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full pt-12">
+        <div name="projects" className="pb-4 pt-32">
+          <p  className="text-4xl font-bold inline border-b-4 text-[#FCFFFC] border-[#248232]">
             Projects
           </p>
-          <p className="pt-6 pb-4">{"// Take a look at my most recent projects"}</p>
+          <p  className="pt-6 pb-4">
+            {"// Take a look at my most recent projects"}
+          </p>
         </div>
 
         {/* Container */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 min-w-0">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-6 min-w-0">
           {/* Grid Project Card */}
           <div className=" bg-[#2D3A3A] flex flex-col justify-between hover:scale-105 transition-transform duration-300">
             {/* Image */}
@@ -42,22 +43,20 @@ export const Projects = () => {
 
               {/* buttons */}
               <div className="pt-8 flex flex-row justify-center space-x-4 ">
-
-              <a
-              href="https://nc-games1334.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button
-                className=" text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
+                <a
+                  href="https://nc-games1334.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className=" text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
                   text-[#FCFFFC] group border-2 px-4 py-3 flex 
                   "
-              >
-                Deployed App
-              </button>
-            </a>
+                  >
+                    Deployed App
+                  </button>
+                </a>
 
-                
                 <Link to="ncgames" smooth={true} duration={500}>
                   <button
                     className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
@@ -92,20 +91,19 @@ export const Projects = () => {
 
               {/* buttons */}
               <div className="pt-12 flex flex-row justify-center space-x-4">
-
-              <a
-              href="https://www.youtube.com/watch?v=N_lc2a5umbo&ab_channel=Northcoders"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button
-                className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
+                <a
+                  href="https://www.youtube.com/watch?v=N_lc2a5umbo&ab_channel=Northcoders"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
                   text-[#FCFFFC] group border-2 px-4 py-3 flex
                   "
-              >
-                Video Demo
-              </button>
-            </a>
+                  >
+                    Video Demo
+                  </button>
+                </a>
 
                 <Link to="fanfinder" smooth={true} duration={500}>
                   <button
@@ -140,20 +138,19 @@ export const Projects = () => {
 
               {/* buttons */}
               <div className="pt-12 flex flex-row justify-center space-x-4">
-
-              <a
-              href="https://weather-app-virid-alpha.vercel.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button
-                className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
+                <a
+                  href="https://weather-app-virid-alpha.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
                   text-[#FCFFFC] group border-2 px-4 py-3 flex
                   "
-              >
-                Deployed App
-              </button>
-            </a>
+                  >
+                    Deployed App
+                  </button>
+                </a>
                 <Link to="weather" smooth={true} duration={500}>
                   <button
                     className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
@@ -166,6 +163,55 @@ export const Projects = () => {
               </div>
             </div>
           </div>
+
+           {/* Grid Project Card 4*/}
+           <div className=" bg-[#2D3A3A] flex flex-col justify-between hover:scale-105 transition-transform duration-300">
+            {/* Image */}
+            <img
+              src={weatherappimg}
+              alt="Weather App"
+              className="w-full p-2 pb-3 object-cover"
+            />
+
+            {/* Content */}
+            <div className="flex flex-col justify-between flex-grow px-3 pb-3  ">
+              <span className="text-2xl font-bold text-[#FCFFFC] tracking-wider ">
+                Weather App
+              </span>
+              <p className="pb-3">
+                A web app built with React and Typescript to fetch and display
+                weather data using the OpenWeather API.
+              </p>
+
+              {/* buttons */}
+              <div className="pt-12 flex flex-row justify-center space-x-4">
+                <a
+                  href="https://weather-app-virid-alpha.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
+                  text-[#FCFFFC] group border-2 px-4 py-3 flex
+                  "
+                  >
+                    Deployed App
+                  </button>
+                </a>
+                <Link to="weather" smooth={true} duration={500}>
+                  <button
+                    className="text-center mb-2 hover:bg-[#2BA84A] hover:border-[#2BA84A] duration-300 
+                  text-[#FCFFFC] group border-2 px-4 py-3 flex
+                  "
+                  >
+                    Learn More...
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          
 
           {/*container end*/}
         </div>
